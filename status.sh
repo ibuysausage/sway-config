9cf2a29d3b5351e9b9d063a71677897d3de6e4de
ibuysausage <bannansmooth68@proton.me>
2026-07-18 08:38:21 -0400

    update swaybar

--- status.sh
+++ status.sh
@@ -2,13 +2,21 @@
 # You should see changes to the status bar after saving this script.
 # If not, do "killall swaybar" and $mod+Shift+c to reload the configuration.
 
+sep='<span weight="bold" foreground="#00ff00">|</span>'
+
 uptime=$(uptime | cut -d "," -f 1 | cut -d " " -f 4-6)
 
-date=$(date "+%a %F %H:%M")
+date=$(date "+%a %F") 
 
+time=$(date "+%H:%M:%S")
+
+# Works on FreeBSD
+# Not sure for linux
 sys_info=$(uname -ar | cut -d " " -f 1,3 | cut -d "-" -f 1)
 
 battery_status=$(acpiconf -i 0 | grep "Remaining capacity" | cut -b 21-23)
 
-# Emojis and characters for the status bar
-echo -e "|  $sys_info  |  $uptime  |  $date  |  $battery_status  |"
+# ifconfig is used on FreeBSD
+private_ip=$(ifconfig wlan0 | grep "inet" | cut -d " " -f 2)
+
+echo "$sep  $sys_info  on  $private_ip  $sep  $uptime  $sep  $date  $sep  $battery_status  $sep  $time  $sep"
