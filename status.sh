acf1ac302b39d6e60a962d7a434432e8e296b067
clippyricer <bannansmooth68@proton.me>
2026-07-20 16:47:46 -0400

    add awk and finish linux support

--- status.sh
+++ status.sh
@@ -10,20 +10,18 @@ date=$(date "+%a %F")
 
 time=$(date "+%H:%M:%S")
 
-# Works on FreeBSD
-# Not sure for linux
 sys_info=$(uname -ar | cut -d " " -f 1,3 | cut -d "-" -f 1)
 
 if [[ $OSTYPE == "freebsd"* ]]; then
-    battery_status=$(acpiconf -i 0 | grep "Remaining capacity" | cut -b 21-23)
+    battery_status="$(acpiconf -i 0 | grep 'Remaining capacity' | awk -F' ' '{print $3}') $(acpiconf -i 0 | grep "State" | awk -F' ' '{print $2}'"
 else
     battery_status="$(cat $bat/capacity)% <span weight=\"bold\">$(cat $bat/status)</span>"
 fi
 
 if [[ $OSTYPE == "freebsd"* ]]; then
-    private_ip=$(ifconfig wlan0 | grep "inet" | cut -d " " -f 2)
+    private_ip=$(ifconfig wlan0 | grep 'inet' | cut -d ' ' -f2)
 else
-    private_ip=$(ip address | grep "wlan0$" | cut -d " " -f 6)
+    private_ip=$(ip address | grep 'wlan0$' | cut -d ' ' -f6)
 fi
 
 echo "$sep  $sys_info  $sep  $private_ip  $sep  $date  $sep  $battery_status  $sep  $time  $sep"
