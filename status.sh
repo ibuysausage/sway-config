2974b79626472639f477b6294c0c6f3e7dee44d2
clippyricer <bannansmooth68@proton.me>
2026-07-20 16:22:19 -0400

    battery support linux

--- status.sh
+++ status.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
 # The Sway configuration file in ~/.config/sway/config calls this script.
 # You should see changes to the status bar after saving this script.
 # If not, do "killall swaybar" and $mod+Shift+c to reload the configuration.
 
 sep='<span weight="bold" foreground="#00ff00">|</span>'
+bat="/sys/class/power_supply/BAT0"
 
 date=$(date "+%a %F") 
 
@@ -15,10 +17,13 @@ sys_info=$(uname -ar | cut -d " " -f 1,3 | cut -d "-" -f 1)
 if [[ $OSTYPE == "freebsd"* ]]; then
     battery_status=$(acpiconf -i 0 | grep "Remaining capacity" | cut -b 21-23)
 else
-    battery_status="$(cat /sys/class/power_supply/BAT0/capacity)%"
+    battery_status="$(cat $bat/capacity)% <span weight=\"bold\">$(cat $bat/status)</span>"
 fi
 
-# ifconfig is used on FreeBSD
-private_ip=$(ifconfig wlan0 | grep "inet" | cut -d " " -f 2)
+if [[ $OSTYPE == "freebsd"* ]]; then
+    private_ip=$(ifconfig wlan0 | grep "inet" | cut -d " " -f 2)
+else
+    private_ip=$(ip address | grep "wlan0$" | cut -d " " -f 6)
+fi
 
 echo "$sep  $sys_info  $sep  $private_ip  $sep  $date  $sep  $battery_status  $sep  $time  $sep"
